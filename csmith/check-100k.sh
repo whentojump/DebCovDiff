4677c7d41483bf7760ce96cd8d8999c02d44bfc4
Wentao Zhang <zhangwt1997@gmail.com>
2025-10-09 03:06:48 -0500

    csmith: verify checksum

--- csmith/check-100k.sh
+++ csmith/check-100k.sh
@@ -1,3 +1,28 @@
+checksum_failed() {
+    cat << EOF
+Unexpected program checksum. Examine program generation log and see if any
+segmentation fault happened. Try regenerating them with:
+
+    csmith -s <SEED> [--inline-function] [--lang-cpp] > <FILENAME>
+
+Or completely rerun gen.py with a lower level of parallelism.
+EOF
+    exit 1
+}
+
+SUM=`(cd default-100k && find . -type f -exec md5sum {} + | LC_ALL=C sort | md5sum)`
+if [[ $SUM != b065d1f68a4945dab0e23b63ba9b414e* ]]; then
+    checksum_failed
+fi
+SUM=`(cd inline-100k && find . -type f -exec md5sum {} + | LC_ALL=C sort | md5sum)`
+if [[ $SUM != fdfd268963e48bcaaf4566318b59463d* ]]; then
+    checksum_failed
+fi
+SUM=`(cd cpp-100k && find . -type f -exec md5sum {} + | LC_ALL=C sort | md5sum)`
+if [[ $SUM != cc8157e7a22c83799cc9c189ca42a392* ]]; then
+    checksum_failed
+fi
+
 for d in default-100k inline-100k cpp-100k; do
     python check.py --nproc=40 --csmith-programs-dir $d |& tee $d.log
 done
--- csmith/check-1k.sh
+++ csmith/check-1k.sh
@@ -1,3 +1,28 @@
+checksum_failed() {
+    cat << EOF
+Unexpected program checksum. Examine program generation log and see if any
+segmentation fault happened. Try regenerating them with:
+
+    csmith -s <SEED> [--inline-function] [--lang-cpp] > <FILENAME>
+
+Or completely rerun gen.py with a lower level of parallelism.
+EOF
+    exit 1
+}
+
+SUM=`(cd default-1k && find . -type f -exec md5sum {} + | LC_ALL=C sort | md5sum)`
+if [[ $SUM != 06d3ea664413df70b65ca9fa3044620f* ]]; then
+    checksum_failed
+fi
+SUM=`(cd inline-1k && find . -type f -exec md5sum {} + | LC_ALL=C sort | md5sum)`
+if [[ $SUM != e1a82bdc99f7b0e367a25ae1483d1ab1* ]]; then
+    checksum_failed
+fi
+SUM=`(cd cpp-1k && find . -type f -exec md5sum {} + | LC_ALL=C sort | md5sum)`
+if [[ $SUM != 4ccca1b81e3e2732fd5fe769e85c8198* ]]; then
+    checksum_failed
+fi
+
 for d in default-1k inline-1k cpp-1k; do
     python check.py --nproc=40 --csmith-programs-dir $d |& tee $d.log
 done
